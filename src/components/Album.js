0dc8a0a190ccce8907715481cace99dc4fc7dfeb
stvnlee890 <stvnlee890@gmail.com>
2022-08-05 08:55:56 -0700

    selecting multiple images and putting them in state. Fixed no duplicate items

--- src/components/Album.js
+++ src/components/Album.js
@@ -6,17 +6,22 @@ const Album = () => {
   const navigate = useNavigate()
   const [photos, setPhotos] = useState([])
   const [deleted, setDeleted] = useState('')
- 
+  const [isLoading, setLoading] = useState(false)
+ console.log(isLoading)
+  // handle click to get all images from db
   const handleClick = (event) => {
     event.preventDefault();
     axios.get('http://localhost:4000/photo')
-      .then(res => {
-       setPhotos(res.data)
-      })
+      .then(res => setPhotos(res.data))
   }
+
   const handleDelete = (event) => {
     const id = event.target.id
+    // prevents multiple selection of same id
+    // setState callback creates a persistent value in the state, or else it would update everytime to 
+    // the new item we added. 
     setDeleted((prev) => !deleted.includes(id) ? [...prev, id] : deleted)
+   
   }
 
 console.log(deleted)
@@ -49,22 +54,3 @@ return (
 export default Album
 
 
-{/* <div>
-<h1>
-  Album component
-</h1>
-<div>
-  {photos.map((photo) => (
-    <>
-      <img 
-      src={'https://i.gifer.com/Nutm.gif'} 
-      key={photo.id} 
-      alt={photo.filename}
-      />
-      <p>
-        {photo.caption}
-      </p>
-    </>
-  ))}
-</div>
-</div> */}

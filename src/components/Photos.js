464c816bb84beb5075ee6a627ace6a42829d7615
go4tsy <mosesetienne@hotmail.com>
2022-08-08 13:34:41 -0400

    ...

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -16,7 +16,7 @@ const Photos = () => {
     // handle click to get all images from db
     const handleClick = (event) => {
         event.preventDefault();
-        axios.get('http://localhost:8000/albums')
+        axios.get(`http://localhost:8000//${albumId}/photos`)
             .then(res => setPhotos(res.data))
         console.log(photos)
     }

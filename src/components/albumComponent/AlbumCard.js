1bd539d8d2e63b2466710b05a22c3ff568614ff4
stvnlee890 <stvnlee890@gmail.com>
2022-08-08 11:46:54 -0700

    bootstrap changes

--- src/components/albumComponent/AlbumCard.js
+++ src/components/albumComponent/AlbumCard.js
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button'
+
 // each individual album
 const AlbumCard = ({ id, albumName }) => {
   const navigate = useNavigate()
@@ -12,11 +14,13 @@ const AlbumCard = ({ id, albumName }) => {
   }
   return (
     <>
-    <Container className='albumContainer'>
+    {/* <Container className='albumContainer'> */}
     <div>
       <Card style={{ width: '18rem' }}>
-        <Card.Body className='img-container'>
-      <p onClick={handleClick} id={id}>
+        <Card.Body className='album-img-container'>
+        <Card.Title>Card Title1</Card.Title>
+        <Button onClick={handleClick} id={id} variant='primary'>View Album</Button>
+      <p>
         {albumName}
       </p>
       <h3>
@@ -25,9 +29,25 @@ const AlbumCard = ({ id, albumName }) => {
         </Card.Body>
       </Card>
     </div>
-    </Container>
+    {/* </Container> */}
     </>
   )
 }
 
 export default AlbumCard
+
+
+
+
+
+
+
+
+
+{/* <Card style={{ width: '18rem' }}>
+            <Card.Body className='album-img-container'>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Title>Card Title1</Card.Title>
+              <Button  onClick={handleClick} variant="primary"> View Album</Button>
+            </Card.Body>
+          </Card> */}

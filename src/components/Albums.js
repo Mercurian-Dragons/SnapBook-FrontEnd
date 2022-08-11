f5854f54cd6bda71a446950aa76f3ac8d051230a
stvnlee890 <stvnlee890@gmail.com>
2022-08-10 20:21:30 -0700

    made save in Album

---
+++ src/components/Albums.js
@@ -0,0 +1,106 @@
+import {  useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import CreateAlbum from './albumComponent/CreateAlbum';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faSquareShareNodes, faPencil, faLock, faLink, faArrowLeft, faHouse } from "@fortawesome/free-solid-svg-icons"
+
+
+
+const Albums = () => {
+ 
+  const navigate = useNavigate()
+  // const [albumPhotos, setAlbumPhotos] = useState([])
+
+    // handle click to get all images from db
+    const handleClick = (event) => {
+      event.preventDefault();
+  
+      navigate("/photos")
+      // change to album 
+    }
+    
+
+  // useEffect(() => {
+  // }, [albumPhotos])
+
+  return (
+    <>
+      <div className='albumTopContainer'>
+        <FontAwesomeIcon icon={faHouse} className='logos' />
+        <div>
+          <div className='logosContainer'>
+            <h1>
+              <FontAwesomeIcon icon={faArrowLeft} className='logos' />
+              Album's info here
+            </h1>
+            <FontAwesomeIcon icon={faStar} className='logos' />
+            <FontAwesomeIcon icon={faPencil} className='logos' />
+            <FontAwesomeIcon icon={faSquareShareNodes} className='logos' />
+            <FontAwesomeIcon icon={faLock} className='logos' />
+            <FontAwesomeIcon icon={faLink} className='logos' />
+
+          </div>
+
+        </div>
+
+        <Container className='albumContainer'>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body className='album-img-container'>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Title>Card Title1</Card.Title>
+              <Button  onClick={handleClick} variant="primary"> View Album</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body className='album-img-container'>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Title>Card Title2</Card.Title>
+              <Button  onClick={handleClick} variant="primary"> View Album</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body className='album-img-container'>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Title>Card Title3</Card.Title>
+              <Button  onClick={handleClick} variant="primary"> View Album</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body className='album-img-container'>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Title>Card Title4</Card.Title>
+              <Button  onClick={handleClick} variant="primary"> View Album</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body className='album-img-container'>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Title>Card Title5</Card.Title>
+              <Button  onClick={handleClick} variant="primary"> View Album</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body className='album-img-container'>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Title>Card Title6</Card.Title>
+              <Button  onClick={handleClick} variant="primary">  View Album</Button>
+            </Card.Body>
+          </Card>
+
+        </Container>
+       
+      
+      </div>
+    </>
+
+  )
+}
+
+
+
+export default Albums
+
+

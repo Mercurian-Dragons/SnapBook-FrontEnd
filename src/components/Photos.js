876c94c46455c96f0bbcf20c9b38744b2912b398
stvnlee890 <stvnlee890@gmail.com>
2022-08-10 21:16:18 -0700

    figured out error handling for image submissions. must include certain extentions before being submitted

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -39,11 +39,7 @@ const Photos = () => {
   //   const handleSelect = (selectedIndex, e) => {
   //     setIndex(selectedIndex);
   // };
-<<<<<<< HEAD
-
-=======
   
->>>>>>> made minor changes
   const photoViewerClick = (event) => {
     event.preventDefault()
     setDeletePhoto(event.target.id)
